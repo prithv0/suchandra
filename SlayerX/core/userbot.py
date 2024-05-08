7bf948c337dd8f230554fcb89ab74208b3a76308
ShubhoEditx <166376289+ShubhoEditx@users.noreply.github.com>
2024-05-08 13:31:54 +0530

    Update userbot.py

--- SlayerX/core/userbot.py
+++ SlayerX/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("Learningbots79")
-                await self.one.join_chat("Learning_bots")
+                await self.one.join_chat("TheBongFamily_Chatt")
+                await self.one.join_chat("about_shubho")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("Learningbots79")
-                await self.one.join_chat("learning_bots")
+                await self.two.join_chat("TheBongFamily_Chatt")
+                await self.one.join_chat("about_shubho")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("Learningbots79")
-                await self.one.join_chat("Learning_bots")
+                await self.three.join_chat("TheBongFamily_Chatt")
+                await self.one.join_chat("about_shubho")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("Learningbots79")
-                await self.one.join_chat("learning_bots")
+                await self.four.join_chat("TheBongFamily_Chatt")
+                await self.one.join_chat("about_shubho")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("learningbots79")
-                await self.one.join_chat("learning_bots")
+                await self.five.join_chat("TheBongFamily_Chatt")
+                await self.one.join_chat("about_shubho")
             except:
                 pass
             assistants.append(5)
